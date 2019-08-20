51ea60bd1c1849fab8af804bc31def45bd2f449e
ashibata <dontsendmeanymail@gmail.com>
2019-08-20 22:42:58 +0900

    add pid control

--- temp.py
+++ temp.py
@@ -5,13 +5,14 @@ import json
 import os
 import RPi.GPIO as GPIO
 import threading
-from time import sleep
+import time
 
 class Temp:
 	device_files = glob.glob('/sys/bus/w1/devices/*/w1_slave')
 	device_file = ""
 	temp_json   = {}
 	running     = True
+	GPIO_RELAY  = 21
 
 	def __init__(self, target_temp):
 		if target_temp < 1 or 100.0 < 100.0:
@@ -23,7 +24,7 @@ class Temp:
 		Temp.device_file = Temp.device_files[0]
 		Temp.target_temp = target_temp
 		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(21, GPIO.OUT)
+		GPIO.setup(Temp.GPIO_RELAY, GPIO.OUT)
 		os.system('modprobe w1-gpio')
 		os.system('modprobe w1-therm')
 		Temp.lock   = threading.Lock()
@@ -56,18 +57,27 @@ class Temp:
 		return data
 
 	def keep_control_temp(self):
+		hist = []
+		prev = 0
+		t = 0
 		try:
 			while Temp.running:
 				now = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
 				with Temp.lock:
 					Temp.temp_json[now] = self.read_temp()
-				if Temp.temp_json[now] < Temp.target_temp:
-					GPIO.output(21, GPIO.HIGH)
-					low_high = 'GPIO.HIGH'
-				else:
-					GPIO.output(21, GPIO.LOW)
-					low_high = 'GPIO.LOW'
-				print(now, Temp.target_temp, Temp.temp_json[now], low_high)
+				hist.insert(0, Temp.temp_json[now])
+				if len(hist) > 7:
+					hist.pop()
+				pg = self.p(Temp.temp_json[now], Temp.target_temp, 3)
+				ig = self.i(hist, Temp.target_temp, 0.01)
+				power = pg + ig
+				if power > 0:
+					power += 0.1
+				prev = Temp.temp_json[now]
+				print t, Temp.temp_json[now], power
+				self.output(power)
+				t += 10
+				print(now, Temp.target_temp, Temp.temp_json[now])
 		finally:
 			self.stop_control_temp()
 			print'Temp control_temp method finally worked'
@@ -76,3 +86,30 @@ class Temp:
 		Temp.running = False
 		GPIO.cleanup()
 		print 'Temp stop_control_temp method worked'
+
+	def output(self, power):
+		if power > 1:
+			power = 1
+		on = power * 10
+		off = (1 - power) * 10
+		if on > 0:
+			GPIO.output(Temp.GPIO_RELAY, GPIO.HIGH)
+			time.sleep(on)
+		if off > 0:
+			GPIO.output(Temp.GPIO_RELAY, GPIO.LOW)
+			time.sleep(off)
+
+	def p(self, temp, target, kp):
+		d = target - temp
+		if d < 0:
+			return 0
+		power = d / target * kp
+		return power
+
+	def i(self, hist, target, ki):
+		s = 0
+		for i in range(len(hist)-1):
+			d1 = target - hist[i]
+			d2 = target - hist[i+1]
+			s += (d1 + d2) / 2 * ki
+		return s
